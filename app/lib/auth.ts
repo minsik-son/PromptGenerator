b71798323cd647362ea144fb228f5c099462ff58
Minsik Son <minsik.son@gmail.com>
2025-02-22 00:27:12 -0800

    배포수정9

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,7 +1,7 @@
-import type { NextAuthConfig } from "next-auth"
+import type { AuthOptions  } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions: NextAuthConfig = {
+export const authOptions: AuthOptions  = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
